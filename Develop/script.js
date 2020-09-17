c51f5fe1deea898e4f1ca9a406d5ff3344b42d6f
jgarcia60 <68722324+jgarcia60@users.noreply.github.com>
2020-09-17 11:27:30 -0400

    defined arrays for all possible characters, set up prompts for user, and conditionals before the loop

--- Develop/script.js
+++ Develop/script.js
@@ -10,5 +10,43 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  var characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+  'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+  var numbers =['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; 
+  var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '+', '_', '`', '~', '<', '>', '/', ';', ':', "'", '.', '[', ']', '{', '}'];
+
+ 
+  var validLength = false;
+
+  while (validLength !== true) {
+    var passwordLength = prompt("How long do you want your password to be?");
+    var passwordLength = parseInt(passwordLength);
+    if (passwordLength < 8 || passwordLength > 128) {
+      alert("Sorry, you must choose a password of at least 8 characters in length but less than 128 characters");
+    } else validLength = true;
+  }
+  var includeNumbers = confirm("Do you want your password to include numbers?");
+  var includeSpecialChars = confirm("Do you want your password to include special chracters?");
+  var includeUpperCase = confirm("Do you want your password to include upper case letters?");
+  var password = "";
+  var randomGeneratorLimit;
+
+  if (includeNumbers && includeSpecialChars && includeUpperCase || includeNumbers && includeSpecialChars) {
+    randomGeneratorLimit = characters.length + numbers.length + specialCharacters.length;
+  } else if (includeUpperCase && includeSpecialChars) {
+    randomGeneratorLimit = characters.length + specialCharacters.length;
+  } else if (includeSpecialChars && includeUpperCase) {
+    randomGeneratorLimit = characters.length + specialCharacters.length;
+  }
+
+// need a conditional in the for loop for a 50/50 random generator for upper case, if the random number is within the characters length 
+  for (var i = 0; i < passwordLength; i++) {
+    var indexOfPassword = Math.floor(Math.random()*)
+  }
+  
+
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
